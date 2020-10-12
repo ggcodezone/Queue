a345a28d701dc2067dac95aad1b2614dc4bb206b
Dank <ggkalasky@gmail.com>
2020-10-11 22:24:48 -0400

    Work in progress!

---
+++ Old Ver. (messed up code lol)/Queue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                int[] getRandomArray(int maxSize, int Min, int Max)
+                {
+
+                    int[] tempArray = new int[maxSize];
+                    Random randNum = new Random();
+                    for (int i = 0; i < maxSize; i++)
+                    {
+                        tempArray[i] = randNum.Next(Min, Max);
+                    }
+                    return tempArray;
+                }
+
+
+                /*
+                 * Below is just a test to reflect out a range of the array so that you can see if it worked. 
+                 * Over time you may want to pull this out or keep it it does not hurt.  
+                 */
+                void reflectArrayRange(int[] tempArray, int fromPtr, int backPtr, string message)
+                {
+                    Console.WriteLine(message);
+                    for (int i = fromPtr; i < backPtr; i++)
+                    {
+                        Console.WriteLine(tempArray[i]);
+                    }
+                    Console.ReadLine(); // works up to here.  Stops running for testing thats all
+                    var queue = new Queue(tempArray);
+                }
+
+            }
+           
+        }
+    }
+}
---
+++ Queue-app/Queue-app/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Queue_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(); //Object creates random number
+            int[] array = new int[100]; //This sets the size of the array
+            for (int i = 0; i < array.Length; i++) 
+            {
+                array[i] = random.Next(1, 100); //this for loop is used to set the range for the value of the random numbers
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]); //this for loop prints out the array so we can check the contents 
+            }
+            Queue numberQueue = new Queue();
+            numberQueue.Enqueue(array); //adding the array into the Queue
+        }
+
+    }
+}
