08339dac8a99f10118d208f1d0c7adc5fc8f2f06
Dank <ggkalasky@gmail.com>
2020-10-14 11:52:07 -0400

    Working on adding in pt 2 of the program

--- Queue-app/Queue-app/Program.cs
+++ Queue-app/Queue-app/Program.cs
@@ -7,7 +7,7 @@ namespace Queue_app
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //CREATING QUEUE WITH RANDOM INTEGERS
             Random random = new Random(); //Object creates random number
             int[] array = new int[100]; //This sets the size of the array
             for (int i = 0; i < array.Length; i++) 
@@ -21,6 +21,24 @@ namespace Queue_app
             }
             Queue numberQueue = new Queue();
             numberQueue.Enqueue(array); //adding the array into the Queue
+
+
+            //LOOKING FOR SPECIFIC DATA IN QUEUE
+            //this isn't the right way to do it because it's not working... I don't know how to refer
+            //to the individual postions in the queue (I was thinking "i" but does not work)
+            foreach (int i in numberQueue)
+            { if (i > 3) //Just used this to test the if/else. If I want to do factors of three I'm reading you use % but I'm not entirely sure how
+                {
+                   numberQueue.Dequeue(); 
+                }
+                else
+                {
+                    Console.WriteLine(i); //I want to loop thru the queue; if element meets condition, write it out for user, elsewise dequeue without comment
+                    numberQueue.Dequeue();
+                }
+
+
+            }
         }
 
     }
